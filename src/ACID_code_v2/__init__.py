8c8309a9306f03e354c3af09710fb2bead98e608
benmac <benjamincadell@gmail.com>
2026-03-13 14:52:28 +0000

    Remove _reload_all function as it may mess with some module checks with beartype

--- src/ACID_code_v2/__init__.py
+++ src/ACID_code_v2/__init__.py
@@ -9,17 +9,3 @@ from .data import Data, Config, Linelist
 
 __all__ = ['ACID', 'ACID_HARPS', 'Acid', 'LSD', 'MCMC', 'Result', 'Profiles',
            'utils', 'calc_deltav', 'Data', 'Config', 'Linelist']
-
-def _reload_all():
-    # Reloads all submodules of the ACID_code_v2 package. 
-    # Only useful for development purposes when using ipython.
-    import importlib, sys
-    pkg = __name__  # 'ACID_code_v2'
-
-    # reload submodules
-    for name, module in list(sys.modules.items()):
-        if name.startswith(pkg + ".") and module is not None:
-            importlib.reload(module)
-
-    # reload top-level package last
-    importlib.reload(__import__(pkg))
