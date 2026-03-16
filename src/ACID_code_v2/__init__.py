4d473c8d0094ea32902b6525b0463ec780ecb346
benmac <benjamincadell@gmail.com>
2026-03-16 14:42:15 +0000

    Update init with new classes

--- src/ACID_code_v2/__init__.py
+++ src/ACID_code_v2/__init__.py
@@ -5,7 +5,8 @@ from . import utils
 from .profiles import Profiles
 from .mcmc import MCMC
 from .utils import calc_deltav # for legacy reasons
-from .data import Data, Config, LineList
+from .data import Data, DataList, Config, LineList, MaskingLines
 
 __all__ = ['ACID', 'ACID_HARPS', 'Acid', 'LSD', 'MCMC', 'Result', 'Profiles',
-           'utils', 'calc_deltav', 'Data', 'Config', 'LineList']
+           'utils', 'calc_deltav', 'Data', 'Config', 'LineList', 'MaskingLines',
+           'DataList']
