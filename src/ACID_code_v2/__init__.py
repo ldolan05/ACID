c85ace5bce520357b39819df28148ea245059fde
benpc_wsl <benjamincadell@gmail.com>
2026-03-16 00:21:52 +0000

    Update init with new LineList class name

--- src/ACID_code_v2/__init__.py
+++ src/ACID_code_v2/__init__.py
@@ -5,7 +5,7 @@ from . import utils
 from .profiles import Profiles
 from .mcmc import MCMC
 from .utils import calc_deltav # for legacy reasons
-from .data import Data, Config, Linelist
+from .data import Data, Config, LineList
 
 __all__ = ['ACID', 'ACID_HARPS', 'Acid', 'LSD', 'MCMC', 'Result', 'Profiles',
-           'utils', 'calc_deltav', 'Data', 'Config', 'Linelist']
+           'utils', 'calc_deltav', 'Data', 'Config', 'LineList']
