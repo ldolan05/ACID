91b17b666bce6b0ceb6e3bb83424f8555d26bd07
benpc_wsl <benjamincadell@gmail.com>
2026-09-19 13:02:14 +0100

    Apply new logger to acid.py

--- src/ACID_code/diagnostics/errors.py
+++ src/ACID_code/diagnostics/errors.py
@@ -5,11 +5,23 @@ Custom error classes for ACID. To be used mainly for handling specific errors in
 class ACIDError(Exception):
     """Base class for all ACID-related errors."""
 
+class ACIDInputError(ACIDError, ValueError):
+    """Invalid input arguments to ACID."""
+
 class LineListRangeError(ACIDError):
     """Custom error for when no lines in the linelist are within the wavelength range of the observed spectrum."""
 
-class ContinuumError(ACIDError):
+class ContinuumFitError(ACIDError):
     """Custom error for when the continuum fit results in negative fluxes or errors."""
 
 class SNCutError(ACIDError):
     """Custom error for when the S/N cut results in no valid pixels."""
+
+class InitialStateError(ACIDError):
+    """Custom error for when the initial state for MCMC walkers is invalid, or the process of generating it fails."""
+
+class ResultError(ACIDError):
+    """Custom error for when there is an issue with the results processing in the Result class."""
+
+class ACIDStateError(ACIDError):
+    """Similar to ``ACIDStateWarning``, but the the issue cannot be continued from."""
