4221fe6a87f08fe778819e06edf3cdbb50cc2030
benmac <benjamincadell@gmail.com>
2026-09-21 16:03:39 +0100

    Remove __all__ designation in data directory init

--- src/ACID_code/data/__init__.py
+++ src/ACID_code/data/__init__.py
@@ -3,11 +3,3 @@ from .data import Data
 from .data_list import DataList
 from .line_list import LineList
 from .masking_lines import MaskingLines
-
-__all__ = [
-    "Config",
-    "Data",
-    "DataList",
-    "LineList",
-    "MaskingLines",
-]
